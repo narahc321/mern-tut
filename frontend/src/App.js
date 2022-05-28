2b92dba3d653b6438268aec0e711684213e33750
Sai Charan Teja Tanguturu <tanguturucharan@gmail.com>
2022-05-28 21:18:22 +0530

    feat: more features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,30 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+
+import MainNavigation from './shared/components/navigation/MainNavigation';
 import Places from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import Users from './user/pages/Users';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places" exact>
-          <Places />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new">
+            <Places />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
