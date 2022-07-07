7f90bc0dff2b3de64323f3883f602dece28a60db
Sai Charan Teja Tanguturu <tanguturucharan@gmail.com>
2022-07-07 17:50:37 +0530

    feat: added frontend in same

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -32,8 +33,12 @@ app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  throw new HttpError('Could not find this route', 404);
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+// app.use((req, res, next) => {
+//   throw new HttpError('Could not find this route', 404);
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
---
+++ backend/public/precache-manifest.bfc675a16a244903d28e383ac771bbf2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd648a1abf58e8997858a3e81ba3671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb66299257afed451b4",
+    "url": "/static/css/3.21314756.chunk.css"
+  },
+  {
+    "revision": "f9e7d2136b948e18116a",
+    "url": "/static/css/4.64d131ef.chunk.css"
+  },
+  {
+    "revision": "aa066554b8129cfa5c8b",
+    "url": "/static/css/5.6644edc6.chunk.css"
+  },
+  {
+    "revision": "d55c0089841ff466505a",
+    "url": "/static/css/6.69e31933.chunk.css"
+  },
+  {
+    "revision": "c1560baf64d5e22777ec",
+    "url": "/static/css/7.9f914307.chunk.css"
+  },
+  {
+    "revision": "3e17a496a7deab07c12f",
+    "url": "/static/css/main.d7bb9742.chunk.css"
+  },
+  {
+    "revision": "f024e31119da304d6db5",
+    "url": "/static/js/2.e40a2c4e.chunk.js"
+  },
+  {
+    "revision": "afb66299257afed451b4",
+    "url": "/static/js/3.bfe186ff.chunk.js"
+  },
+  {
+    "revision": "f9e7d2136b948e18116a",
+    "url": "/static/js/4.4e0fd4ce.chunk.js"
+  },
+  {
+    "revision": "aa066554b8129cfa5c8b",
+    "url": "/static/js/5.0f948ce0.chunk.js"
+  },
+  {
+    "revision": "d55c0089841ff466505a",
+    "url": "/static/js/6.2e4482df.chunk.js"
+  },
+  {
+    "revision": "c1560baf64d5e22777ec",
+    "url": "/static/js/7.dfae2aec.chunk.js"
+  },
+  {
+    "revision": "3e17a496a7deab07c12f",
+    "url": "/static/js/main.801eaeb9.chunk.js"
+  },
+  {
+    "revision": "7d0ac662da2fe675d67e",
+    "url": "/static/js/runtime-main.eccc665a.js"
+  }
+]);
